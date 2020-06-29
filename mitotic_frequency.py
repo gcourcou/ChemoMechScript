737050c332511e1080f0b5dbb46c661a8dfb293c
xcricardo <51096518+xcricardo@users.noreply.github.com>
2020-06-29 16:14:20 -0700

    Update mitotic_frequency.py

--- mitotic_frequency.py
+++ mitotic_frequency.py
@@ -9,20 +9,30 @@ def  master(file="script_out.txt",division_type="uniform") :
     #dict_from_file["mitotic position"]
     #dict_from_file["MF position"]
     #dict_from_file["L"]
-
-    skip_frame=0
+    # skip initial frames
+    skip_frame_start=0
+    # skip final frames
+    skip_frame_end=0
+    
+    for i in range(0,len(dict_from_file["mitotic position"])):
+        if dict_from_file["MF position"][i]==0.0 and dict_from_file["MF position"][i+1]!=0.0:
+            skip_frame_start=i
+    
+    for i in range(1,len(dict_from_file["mitotic position"])-1):
+        if dict_from_file["MF position"][-i]==0.0 and dict_from_file["MF position"][-i-1]!=0.0:
+            skip_frame_end=i
     # If we do not put this filter, then skip_frame can be equal to the whole simulation since MF=0.0 when it 
     # reaches the anterior boundary
-    false_skips_due_to_end_filter=30
-    for i in range(0,len(dict_from_file["mitotic position"])):
-        if dict_from_file["MF position"][i]==0.0 and  i<false_skips_due_to_end_filter :
-            skip_frame=i
+#    false_skips_due_to_end_filter=30
+#    for i in range(0,len(dict_from_file["mitotic position"])):
+#        if dict_from_file["MF position"][i]==0.0 and  i<false_skips_due_to_end_filter :
+#            skip_frame=i
         
         #print("skipping frames " + str(skip_frame))
     mitotic_cell_number = [0,0,0,0,0,0,0,0,0,0]
     mitotic_frequency = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
     #skip_frame+=10
-    for i in range(skip_frame+1,len(dict_from_file["mitotic position"])):
+    for i in range(skip_frame_start+1,len(dict_from_file["mitotic position"])-skip_frame_end):
         for j in range(0,len(dict_from_file["mitotic position"][i])):
             if dict_from_file["mitotic position"][i][j]!=[0.,0.]:
                 Lap=dict_from_file["MF position"][i]-dict_from_file["L"][i][1]
@@ -41,7 +51,3 @@ def  master(file="script_out.txt",division_type="uniform") :
     plt.xlabel("Position (x/$L_{AP}$) ",fontsize=20)
     plt.ylabel("Mitotic Frequency",fontsize=20)
     plt.savefig("mitotic_frequency_"+str(division_type)+".png",bbox_inches='tight',dpi=400)
-    
-    
-    
-master(file="script_out.txt",division_type="uniform")
