26928d2ac323795b30646aa7cef427d7b2183e73
xcricardo <51096518+xcricardo@users.noreply.github.com>
2020-07-02 12:23:54 -0700

    Create P_A_area_plot.py

---
+++ P_A_area_plot.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+np.pi
+
+def  master_P_A_area(file="script_out.txt",division_type="uniform") :
+    with open(file) as f:  
+        dict_from_file = eval(f.read()) 
+    
+
+
+    plt.figure(figsize=(10,5),dpi=400)
+    
+    plt.plot(dict_from_file["Posterior area"])
+        
+    plt.title("Posterior area")
+    plt.xlabel("time",fontsize=20)
+    plt.ylabel("P_area",fontsize=20)
+    plt.savefig("P_area"+str(division_type)+".png",bbox_inches='tight',dpi=400)
+    plt.close()
+    
+    plt.cla()
+    
+    plt.plot(dict_from_file["Anterior area"])
+        
+    plt.title("Anterior area")
+    plt.xlabel("time",fontsize=20)
+    plt.ylabel("A_area",fontsize=20)
+    plt.savefig("A_area"+str(division_type)+".png",bbox_inches='tight',dpi=400)
+    plt.close()
+
+
+    
