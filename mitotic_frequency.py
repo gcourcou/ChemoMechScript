ac4b43a22a503e46a3798fa5cbfaace9f5473ebc
xcricardo <51096518+xcricardo@users.noreply.github.com>
2020-07-01 17:15:31 -0700

    Update mitotic_frequency.py
    using number of slice to suit more general situation

--- mitotic_frequency.py
+++ mitotic_frequency.py
@@ -29,25 +29,39 @@ def  master(file="script_out.txt",division_type="uniform") :
 #            skip_frame=i
         
         #print("skipping frames " + str(skip_frame))
-    mitotic_cell_number = [0,0,0,0,0,0,0,0,0,0]
-    mitotic_frequency = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+    mitotic_cell_number = []
+    mitotic_frequency = []
+    number_of_slice = len(dict_from_file["cell_number_in_strip"][0])
+    for i in range(0, number_of_slice):
+        mitotic_cell_number.append(0)
+        mitotic_frequency.append(0.0)
+
     #skip_frame+=10
     for i in range(skip_frame_start+1,len(dict_from_file["mitotic position"])-skip_frame_end):
         for j in range(0,len(dict_from_file["mitotic position"][i])):
             if dict_from_file["mitotic position"][i][j]!=[0.,0.]:
                 Lap=dict_from_file["MF position"][i]-dict_from_file["L"][i][1]
-                for k in range(0,10):
-                  if (dict_from_file["MF position"][i] - (k+1)*Lap/10) <= dict_from_file["mitotic position"][i][j][0]:
-                    if dict_from_file["mitotic position"][i][j][0] < (dict_from_file["MF position"][i] - k*Lap/10):
+                for k in range(0,number_of_slice):
+                  if (dict_from_file["MF position"][i] - (k+1)*Lap/number_of_slice) <= dict_from_file["mitotic position"][i][j][0]:
+                    if dict_from_file["mitotic position"][i][j][0] < (dict_from_file["MF position"][i] - k*Lap/number_of_slice):
                         mitotic_cell_number[k] += 1
-                for l in range(0,10):
-                  mitotic_frequency[l] = mitotic_cell_number[l] / dict_from_file["cell number in strip"][i][l]
+                for l in range(0,number_of_slice):
+                  mitotic_frequency[l] = mitotic_cell_number[l] / dict_from_file["cell_number_in_strip"][i][l]
     
         
     plt.figure(figsize=(10,5),dpi=400)
     plt.title("mitotic_frequency")
-    plt.plot([0.05,0.15,0.25,0.35,0.45,0.55,0.65,0.75,0.85,0.95],mitotic_frequency)
-    plt.xticks(np.arange(0, 1, step=0.1))
+    x_interval = 1/number_of_slice
+    xlist = []
+    for i in range (0, number_of_slice):
+        xlist.append((0.5+i)*x_interval)
+    
+    plt.plot(xlist,mitotic_frequency)
+    plt.xticks(np.arange(0, 1, step=x_interval))
     plt.xlabel("Position (x/$L_{AP}$) ",fontsize=20)
     plt.ylabel("Mitotic Frequency",fontsize=20)
     plt.savefig("mitotic_frequency_"+str(division_type)+".png",bbox_inches='tight',dpi=400)
+    
+    
+    
+master(file="script_out.txt",division_type="uniform")
