f967080ccdf24ea858ac95eb9bf92239dfe59c4c
George Courcoubetis <courcoub@usc.edu>
2021-04-14 16:58:59 -0700

    Fixed draw specs for color bar in script_uniformgrowth

--- script_uniform_growth.py
+++ script_uniform_growth.py
@@ -12,6 +12,7 @@ warnings.filterwarnings("ignore")
 # add fucntionality of multiple runs in this one do a test of what it is capable of time wize and size wize
 
 import time
+#import tyssue
 
 start = time.time()
 import pandas as pd
@@ -388,8 +389,6 @@ auto_prod_mag = {0: h_auto, 1: []}
 auto_prod_mag[0]=h_auto
 # Cell div pars
 
-# Cell grow and divide params
-growth_control_by = parameters["growth_control_by"]
 
 ## Initialize other cell centric variabls not included in tyssue
 # global parameter for t_mech steps
@@ -529,9 +528,9 @@ def animate_cells2(timer, chem_name, string, plot_time):
     draw_specs["face"]["color_bar"] = True
 
     if chem_name=="area":
-        draw_specs["face"]["color_bar_range"]=[0,0.8]
+        draw_specs["axis"]["color_bar_range"]=[0,0.8]
     else:
-        draw_specs["face"]["color_bar_range"]=False
+        draw_specs["axis"]["color_bar_range"]=False
 
     #    sheet.face_df['visible'] = True
     #    for index,row in sheet.face_df.iterrows():
@@ -726,9 +725,8 @@ def mf_inf_cell(
 
 def visualization(i):
     plot_time=str(np.around(i*conversion_t_hr*t_plot,decimals=2))
-    #animate_cells2(i, "y_concentration", "mech",plot_time)
-#    animate_cells2(i, "area", "mech")
-#    animate_cells2(i, "num_sides", "mech")
+    animate_cells2(i, "y_concentration", "mech",plot_time)
+    animate_cells2(i, "area", "mech",plot_time)
     #animate_cells_MF(i, "y_concentration", "MF",plot_time)
     #plot_chem(i, "y_concentration", "MFvsx",plot_time)
     #plot_chem(i, "area", "areavsx",plot_time)
